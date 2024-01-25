66830bdbfc9278f9d48468697272ee3646da2387
mirza78k <156161852+mirza78k@users.noreply.github.com>
2024-01-25 12:39:34 +0530

    Add files via upload

--- Example.java
+++ Example.java
@@ -1,24 +1,52 @@
-class Example {
-    static void m1(){
-        System.out.println("Static m1 ");
-    }
-      void m2(){
-            System.out.println("Non-Static m1");
-     }
-     public static void main(String[] agrs){
-         //static method  
-         m1(); //we can access Direct with method name
-        Example.m1();// we can access with class name 
-        Example e1 = new Example ();// we can also access by making object 
-        e1.m1();
-        Example e2 = null ; // also access from null referenced
-       e2.m1();
-         // Non-Static
-       //  m2(); CE: non-static method m2() cannot be referenced from a static context
-        //    Example.m2();CE: error: non-static method m2() cannot be referenced from a static context
-               System.out.println();
-         e1.m2();// no CE : , no RE: , because By create object 
-      
-       e2.m2();// no CE: ,RE : java.lang.NullPointerException
-     } 
+package Blocks;
+
+/*public class NonStatic {
+	
+	 * {
+	 *  // non static block is also called instance block \
+	 * } void m1() {
+	 *  { //
+	 * instance block is only at class
+	 *  } 
+	 *  }
+	 
+	
 }
+*/
+/*
+ * class Local_Block{
+ * 
+ * public static void main(String[] args) { { // in main method we create local
+ * block } } }
+ */
+class Example {
+
+	public static void main(String[] args) {
+		System.out.println("NSB");
+		System.out.println("main");
+		new Example(5);
+	}
+	
+
+	Example e1;
+
+	Example() {
+
+		e1 = new Example();
+	}
+
+	Example(int a){
+     		System.out.println("IPC");
+     	}//
+
+	Example(String s){
+     		System.out.println("SPC");
+     	}
+	
+	{
+		System.out.println("hi");
+		 //new Example("MB");
+     
+	}
+}
+
